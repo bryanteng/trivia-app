612b32f62da4168e97a1d2b6da22cea7fb11e79b
Tokuhisa1 <TokuhisaWinston@Outlook.com>
2018-08-23 10:00:29 -0400

    Corrects routing from login

--- trivia-app/app/controllers/sessions_controller.rb
+++ trivia-app/app/controllers/sessions_controller.rb
@@ -5,9 +5,10 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(username: params[:username])
+
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect_to trivia_path
+      redirect_to categories_path
     else
       redirect_to login_path
     end
--- trivia-app/config/routes.rb
+++ trivia-app/config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
-
-  resources :categories
-  resources :trivia
-  resources :users, only: [:new,:create,:show]
   resources :answers
+  resources :categories
+  resources :trivia, only: [:show, :new, :create, :edit, :update, :delete]
+  resources :users
 
   post "/trivia/:id", to: "trivia#trivia_answer", as: "trivia_answer"
 
   get '/login', to: "sessions#new", as: "login"
   post '/login', to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

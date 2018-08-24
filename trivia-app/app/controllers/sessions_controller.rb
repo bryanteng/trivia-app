896b478b42dd46e60ead9bbf81d9224e72d77257
github username <github email address>
2018-08-24 13:55:11 -0400

    added errors for login

--- trivia-app/app/controllers/sessions_controller.rb
+++ trivia-app/app/controllers/sessions_controller.rb
@@ -4,13 +4,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: params[:username])
-
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect_to categories_path
-    else
+    if User.find_by(username: params[:username]).blank?
+      flash[:error] = "Username does not exist"
       redirect_to login_path
+    else
+      @user = User.find_by(username: params[:username])
+
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to categories_path
+      else
+        flash[:error] = "Password is incorrect"
+        redirect_to login_path
+      end
     end
   end
 
